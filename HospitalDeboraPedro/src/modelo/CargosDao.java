f26ec657f3ad4be1885d88da27a4bb0730de3cdb
PEDRO HENRIQUE GRACIANO PEREIRA <15843968650@CAMPUS.MACHADO.MCH>
2025-09-24 08:39:18 -0300

    Signed-off-by: PEDRO HENRIQUE GRACIANO PEREIRA <15843968650@CAMPUS.MACHADO.MCH>

---
+++ HospitalDeboraPedro/src/modelo/CargosDao.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author 15843968650
+ */
+public class CargosDao {
+	
+}
