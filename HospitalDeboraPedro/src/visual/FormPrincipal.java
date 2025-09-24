e561bec86d2798ea43d6d55191741d764613716e
PEDRO HENRIQUE GRACIANO PEREIRA <15843968650@CAMPUS.MACHADO.MCH>
2025-09-24 09:02:27 -0300

    Signed-off-by: PEDRO HENRIQUE GRACIANO PEREIRA <15843968650@CAMPUS.MACHADO.MCH>

--- HospitalDeboraPedro/src/visual/FormPrincipal.java
+++ HospitalDeboraPedro/src/visual/FormPrincipal.java
@@ -9,12 +9,13 @@ package visual;
  *
  * @author 15843968650
  */
-public class FormPrincipal extends javax.swing.JPanel {
+public class FormPrincipal extends javax.swing.JDialog {
 
 	/**
-	 * Creates new form principal
+	 * Creates new form FormPrincipal
 	 */
-	public FormPrincipal() {
+	public FormPrincipal(java.awt.Frame parent, boolean modal) {
+		super(parent, modal);
 		initComponents();
 	}
 
@@ -27,19 +28,85 @@ public class FormPrincipal extends javax.swing.JPanel {
         // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
         private void initComponents() {
 
-                javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-                this.setLayout(layout);
+                jMenuBar1 = new javax.swing.JMenuBar();
+                jMenu1 = new javax.swing.JMenu();
+                jMenuItem1 = new javax.swing.JMenuItem();
+                jMenu2 = new javax.swing.JMenu();
+
+                setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+
+                jMenu1.setText("File");
+
+                jMenuItem1.setText("jMenuItem1");
+                jMenu1.add(jMenuItem1);
+
+                jMenuBar1.add(jMenu1);
+
+                jMenu2.setText("Edit");
+                jMenuBar1.add(jMenu2);
+
+                setJMenuBar(jMenuBar1);
+
+                javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+                getContentPane().setLayout(layout);
                 layout.setHorizontalGroup(
                         layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGap(0, 400, Short.MAX_VALUE)
                 );
                 layout.setVerticalGroup(
                         layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 300, Short.MAX_VALUE)
+                        .addGap(0, 279, Short.MAX_VALUE)
                 );
+
+                pack();
         }// </editor-fold>//GEN-END:initComponents
 
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String args[]) {
+		/* Set the Nimbus look and feel */
+		//<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+		/* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+		 */
+		try {
+			for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					javax.swing.UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (ClassNotFoundException ex) {
+			java.util.logging.Logger.getLogger(FormPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (InstantiationException ex) {
+			java.util.logging.Logger.getLogger(FormPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (IllegalAccessException ex) {
+			java.util.logging.Logger.getLogger(FormPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (javax.swing.UnsupportedLookAndFeelException ex) {
+			java.util.logging.Logger.getLogger(FormPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		}
+		//</editor-fold>
+
+		/* Create and display the dialog */
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				FormPrincipal dialog = new FormPrincipal(new javax.swing.JFrame(), true);
+				dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+					@Override
+					public void windowClosing(java.awt.event.WindowEvent e) {
+						System.exit(0);
+					}
+				});
+				dialog.setVisible(true);
+			}
+		});
+	}
 
         // Variables declaration - do not modify//GEN-BEGIN:variables
+        private javax.swing.JMenu jMenu1;
+        private javax.swing.JMenu jMenu2;
+        private javax.swing.JMenuBar jMenuBar1;
+        private javax.swing.JMenuItem jMenuItem1;
         // End of variables declaration//GEN-END:variables
 }
